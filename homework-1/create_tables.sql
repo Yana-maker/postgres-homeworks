d8f798c4184f2896e6b4037985ba633cf9be81b0
yana <yanaposelenceva@gmail.com>
2023-07-29 21:02:39 +0300

    completed HW1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers(
+	customer_id int primary key,
+	"company_name" varchar(100) NOT NULL,
+	"contact_name" text NOT NULL
+);
+
+CREATE TABLE employees(
+	employee_id INT primary key,
+	"first_name" varchar(100) NOT NULL,
+	"last_name" varchar(100) NOT NULL,
+	"title" varchar(100) NOT NULL,
+	"birth_date" text NOT NULL,
+	"notes" varchar(100) NOT NULL
+);
+
+CREATE TABLE orders(
+    order_id INT primary key,
+    cust_id INT REFERENCES customers(customer_id) NOT NULL,
+    empl_id INT REFERENCES employees(employee_id) NOT NULL,
+    "order_date" text NOT NULL,
+    "ship_city" varchar(100) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,41 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+def read_csv_customers_file():
+    with open ('../homework-1/north_data/customers_data.csv', 'r', encoding='utf-8') as file:
+        data = csv.DictReader(file)
+        to_db = [(i['customer_id'], i['company_name'], i['contact_name']) for i in data]
+        return to_db
+
+def read_csv_employees_file():
+    with open ('../homework-1/north_data/employees_data.csv', 'r', encoding='utf-8') as file:
+        data = csv.DictReader(file)
+        to_db = [(i['employee_id'], i['first_name'], i['last_name'], i['title'], i['birth_date'], i['notes']) for i in data]
+        return to_db
+
+def read_csv_orders_file():
+    with open ('../homework-1/north_data/orders_data.csv', 'r', encoding='utf-8') as file:
+        data = csv.DictReader(file)
+        to_db = [(i['order_id'], i['customer_id'], i['employee_id'], i['order_date'], i['ship_city']) for i in data]
+        return to_db
+
+
+def main():
+    with psycopg2.connect(host='localhost', database='north', user='postgres', password=432502) as conn:
+        with conn.cursor() as cur:
+            cur.executemany('INSERT INTO customers VALUES (%s, %s, %s);', (read_csv_customers_file()))
+            cur.executemany('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s);', (read_csv_employees_file()))
+            cur.executemany('INSERT INTO orders VALUES (%s, %s, %s, %s, %s);', (read_csv_orders_file()))
+            cur.execute('SELECT * FROM customers')
+            cur.execute('SELECT * FROM employees')
+            cur.execute('SELECT * FROM orders')
+
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
