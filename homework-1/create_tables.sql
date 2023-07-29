7de961b4feab8692edaede282bab72bc182034d8
yana <yanaposelenceva@gmail.com>
2023-07-29 21:04:28 +0300

    fixes file

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,24 +1,24 @@
 -- SQL-команды для создания таблиц
 
 CREATE TABLE customers(
-	customer_id int primary key,
-	"company_name" varchar(100) NOT NULL,
+	customer_id text primary key,
+	"company_name" text NOT NULL,
 	"contact_name" text NOT NULL
 );
 
 CREATE TABLE employees(
-	employee_id INT primary key,
-	"first_name" varchar(100) NOT NULL,
-	"last_name" varchar(100) NOT NULL,
-	"title" varchar(100) NOT NULL,
+	employee_id int primary key,
+	"first_name" text NOT NULL,
+	"last_name" text NOT NULL,
+	"title" text NOT NULL,
 	"birth_date" text NOT NULL,
-	"notes" varchar(100) NOT NULL
+	"notes" text NOT NULL
 );
 
 CREATE TABLE orders(
     order_id INT primary key,
-    cust_id INT REFERENCES customers(customer_id) NOT NULL,
+    cust_id text REFERENCES customers(customer_id) NOT NULL,
     empl_id INT REFERENCES employees(employee_id) NOT NULL,
     "order_date" text NOT NULL,
-    "ship_city" varchar(100) NOT NULL
+    "ship_city" text NOT NULL
 )
